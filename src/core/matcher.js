de717de8e6d6167835385b0e32fbeb439530f34b
yu <yul1024@outlook.com>
2026-03-18 13:24:20 +0800

    feat: add core matcher

---
+++ src/core/matcher.js
@@ -0,0 +1,14 @@
+/**
+ * @file 通过选择器选择各种 button 的方法。
+ */
+
+
+/**
+ * 起始界面，选择进入评测。
+ * @returns {Element & Node}
+ */
+const select_enter_button = () => {
+    return document.querySelectorAll("div.tab-item-btn")[0]
+}
+
+
