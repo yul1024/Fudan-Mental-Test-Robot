0858a8367d42e4d3ae8259f394927c8b86d24688
yu <yul1024@outlook.com>
2026-04-14 21:10:40 +0800

    feat: add core checker

--- src/core/checker.js
+++ src/core/checker.js
@@ -1,12 +1,16 @@
 /**
- * 对于状态的检测方法。
+ * @flie 对于状态的检测方法。
  * 冗余检测，不运行脚本依然可以正常运行。
  */
 
 
+/**
+ * 判断当前是否为进入系统的起点。
+ * @returns {boolean}
+ */
 const check_begin_url = () => {
     // 以下 URL 为进入系统的起始。
-    target_url = "http://xlcp.fudan.edu.cn/#/home?isIn=0"
+    const target_url = "http://xlcp.fudan.edu.cn/#/home?isIn=0"
     if (window.location.href === target_url) {
         console.log("Enter system.")
         return true
@@ -17,7 +21,24 @@ const check_begin_url = () => {
 }
 
 
+/**
+ * 检查剩余未做测试的数量。
+ * @returns {NodeListOf<Element>}
+ */
 const check_test_count = () => {
+    const test_count = document.querySelectorAll("span.count")[0].innerText
+    console.log(`Remain test: ${test_count}`)
+    return test_count
+}
 
+
+/**
+ * 检查未做题目的数量。
+ * @returns {NodeListOf<Element>}
+ */
+const check_evaluation_count = () => {
+    const evaluation_count = document.querySelectorAll("div.evaluation-start")
+    console.log(`Remain evaluation: ${evaluation_count}`)
+    return evaluation_count
 }
 
